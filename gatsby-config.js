425a40a8f8ce6c813471cb8d3c6e0a0988b7b385
felixerdy <felix.erdmann@uni-muenster.de>
2019-07-02 21:00:10 +0200

    Blue theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,8 +19,8 @@ module.exports = {
         name: 'Rat Münster',
         short_name: 'Rat Münster',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#005b79',
+        theme_color: '#005b79',
         display: 'minimal-ui',
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
