7f2adc5e864cd2f04dbb1ef2d0833a5b69831558
Thomas Werner <tomsrocket@users.noreply.github.com>
2021-12-22 13:44:07 +0100

    Update 2021 (#34)
    * Basic Script: Read members of "Rat Münster" from oparl and write seats config json
    
    * Beta version of seats html generation
    
    * add more space to the room
    
    * fill out some more seat positions
    
    * party colors and hover effect
    
    * add placeholder image for people without photo
    
    * Add complete Rats Sitzplan layout
    
    * local development instruction
    
    * Design-Update
    
    * Add beta version of sitzplan-html-version

---
+++ 2021/generateSitzplan.py
@@ -0,0 +1,233 @@
+# -*- coding: UTF-8 -*-
+
+import re
+import json
+import logging
+import requests
+import os.path
+
+from datetime import datetime, timedelta
+
+# Basic logger configuration
+logging.basicConfig(level=logging.DEBUG, format='<%(asctime)s %(levelname)s> %(message)s')
+LOGGER = logging.getLogger(__name__)
+TODAY = datetime.now()
+LOGGER.info("=====> CHECK START %s <=====", TODAY)
+CONFIG_MEMBERS = 'config-members.json'
+
+
+# Read core config
+data = []
+with open('config-core.json') as f:
+    config = json.load(f)
+
+print(config)
+
+def getMembers(oparlMembers):
+
+    ## READ ALL MEMBERS FROM OPARL
+    ## AND WRITE config-members.json
+
+    currentDate = '{0}-{1:02d}-{2:02d}'.format(TODAY.year, TODAY.month, TODAY.day)
+    members = []
+
+    for memberUrl in oparlMembers:
+
+        # read membership json from oparl
+        mrq = requests.get(memberUrl)
+        oparlMember = mrq.json()
+        startDate = oparlMember.get('startDate')
+        endDate = oparlMember.get('endDate')
+        personUrl = oparlMember.get('person')
+
+        isCurrentMember = (startDate < currentDate) and ((not endDate) or (endDate > currentDate))
+        LOGGER.debug("%s - %s -- isCurrent: %s -- %s", startDate, endDate, isCurrentMember, personUrl)
+
+        if isCurrentMember:
+            # read person json from oparl
+            prq = requests.get(personUrl)
+            oparlPerson = prq.json()
+
+            pid = 0
+            matchObj = re.search( r'/(\d+)$', personUrl)
+            if matchObj:
+                pid = matchObj.group(1)
+
+            name = oparlPerson.get('name')
+            oparlEmail = oparlPerson.get('email')
+            email = ""
+            party = ""
+            if oparlEmail:
+                email = oparlEmail.pop()
+            if "cdu" in email.lower():
+                party = "CDU"
+            elif "volt" in email.lower():
+                party = "VOLT"
+            elif "gruene" in email.lower():
+                party = "GRÜNE"
+            elif "spd" in email.lower():
+                party = "SPD"
+
+            member = {
+                "name": name,
+                "pid": pid,
+                "party": party,
+                "email": email,
+                "seat": ""
+            }
+            LOGGER.debug("member %s", member)
+
+            members.append(member)
+
+
+    with open(CONFIG_MEMBERS, 'w') as outfile:
+        json.dump(members, outfile)
+
+def writeSitzplanHtml():
+
+    # Read seats config
+    members = []
+    with open('config-members.json') as f:
+        members = json.load(f)
+
+    seats = {}
+    for member in members:
+        seats[member.get('seat')] = member
+
+    sitzplanRows = config.get('roomLayout').get('rows')
+    sitzplanCols = config.get('roomLayout').get('columns')
+
+    html = '''<!doctype html><html lang="de">
+        <meta charset="utf-8">
+        <style>
+            div {{width:{}%;height:70px;border:2x solid #eee}}
+            div.row {{width:100%;border: 1px solid #fefefe;clear: left;}}
+            div.row > div {{float:left}}
+            .occ {{
+                background-color:#ddd;
+                overflow:hidden;
+                border: 1px solid black;
+                cursor: pointer;
+                border-radius: 7px;
+                margin: 0 1px 0 0;
+                background-size: 50px;
+                background-repeat: no-repeat;
+                background-position: 0px center;
+            }}
+            div.occ span {{
+                margin: 4px 4px 0 50px;
+                display: inline-block;
+                font-size: 10pt;
+                overflow-wrap:break-word;
+            }}
+            .p-GRÜNE {{background-color:#64a12d}}
+            .p-LINKE {{background-color:#f39}}
+            .p-VOLT {{background-color:#502379;color:white}}
+            .p-ÖDP {{background-color:#ff6400}}
+            .p-AFD {{background-color:#09f}}
+            .p-CDU {{background-color:#000;color:white}}
+            .p-SPD {{background-color:#E3000F}}
+            .p-FDP {{background-color:#ffdd00}}
+            .p-OHNE {{background-color:#aaa;}}
+            #member img {{
+                max-width:150px;
+                max-height:200px;
+             }}
+            #member {{
+                display:none;
+                position:absolute;
+                overflow:hidden;
+                background-color:yellow;
+                border: 1px solid black;
+                border-radius: 5px;
+                width: 200px;
+                text-align: center;
+                height: 260px;
+            }}
+            #member span {{
+                background-color: orange;
+                display: block;
+                padding: 4px 0;
+            }}
+        </style>
+        <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
+        <body>
+        '''.format(7)
+
+    for row in range(sitzplanRows):
+        html = html + '<div class="row">'
+        for column in range(sitzplanCols):
+            seatId = '{}-{}'.format(row,column)
+            personData = {}
+            if seatId in seats:
+                personData = seats[seatId]
+                pName = personData.get('name')
+                html = html + '<div data-id="{}" data-party="{}" class="occ p-{}" style="{}"><span class="name">{}</span></div>'.format(
+                    personData.get('pid'),
+                    personData.get('party'),
+                    personData.get('party'),
+                    'background-image: url(https://www.stadt-muenster.de/sessionnet/sessionnetbi/im/pe{}.jpg);'.format(personData.get('pid')),
+                    pName if pName else ''
+                    )
+            else:
+                html = html + '<div></div>'
+        html = html + '</div>'
+
+    html = html + '''
+        <div id="member">
+            <span class="name">Name</span>
+            <span class="party">Partei</span>
+            <img class="photo" src="url" />
+        </div>
+        <script>
+            var errCount = 0;
+            $("#member .photo").on('error', function(e) {
+                event.stopPropagation();
+                if (errCount++ <= 1) {
+                    $("#member .photo").attr("src", "img/person.png");
+                }
+            });
+            $("div.occ").click(function(event){
+                const pid = $(this).data("id");
+                location.href="https://www.stadt-muenster.de/sessionnet/sessionnetbi/pe0051.php?__kpenr="+pid;
+            }).hover(function(event) {
+                errCount = 0;
+                const pid = $(this).data("id");
+                const name = $(this).find(".name").text();
+                const party = $(this).data("party");
+                const photoUrl = 'https://www.stadt-muenster.de/sessionnet/sessionnetbi/im/pe'+pid+'.jpg'
+                console.log("id", pid);
+                $("#member .name").html(name);
+                $("#member .party").html(party);
+                $("#member .photo").attr("src", photoUrl);
+                const maxHeight = $(document).height()-300
+                const maxWidth = $(document).width()-220
+                const ypos = (event.pageY > maxHeight) ? maxHeight : event.pageY;
+                const xpos = (event.pageX > maxWidth) ? maxWidth : event.pageX;
+                $("#member").css({top: ypos, left: xpos}).show();
+            }, function() {
+                $("#member").hide();
+            });
+        </script>
+        </body></html>
+        '''
+
+    with open('sitzplan.html', 'w') as outfile:
+        outfile.write(html)
+
+    print(html)
+
+if os.path.isfile(CONFIG_MEMBERS):
+    print("Members config file exists.")
+else:
+    # Read Gremiuem (Rat)
+    gremiumUrl = config.get('core').get('baseurl')
+    LOGGER.debug("Base URL: %s", gremiumUrl)
+
+    r = requests.get(gremiumUrl)
+    gremium = r.json()
+
+    print("Writing members file")
+    getMembers(gremium.get('membership'))
+
+writeSitzplanHtml()
