463402328052c362ef964a3f3c34154a41c21072
Thomas Werner <thomas.w.werner@gmail.com>
2022-10-12 16:47:10 +0200

    Add support for many sitzplaene

--- generateSitzplan.py
+++ generateSitzplan.py
@@ -8,18 +8,21 @@ import os.path
 
 from datetime import datetime, timedelta
 from jinja2 import FileSystemLoader, Environment
+from sympy import true
 
 # Basic logger configuration
-logging.basicConfig(level=logging.DEBUG, format='<%(asctime)s %(levelname)s> %(message)s')
+logging.basicConfig(
+    level=logging.DEBUG, format="<%(asctime)s %(levelname)s> %(message)s"
+)
 LOGGER = logging.getLogger(__name__)
 TODAY = datetime.now()
 LOGGER.info("=====> CHECK START %s <=====", TODAY)
-CONFIG_MEMBERS = 'config-members.json'
+DEFAULT_CONFIG_MEMBERS = "config-members.json"
 
 
 # Read core config
 data = []
-with open('config-core.json') as f:
+with open("config-core.json") as f:
     config = json.load(f)
 
 print(config)
@@ -30,7 +33,7 @@ def getMembers(oparlMembers):
     ## READ ALL MEMBERS FROM OPARL
     ## AND WRITE config-members.json
 
-    currentDate = '{0}-{1:02d}-{2:02d}'.format(TODAY.year, TODAY.month, TODAY.day)
+    currentDate = "{0}-{1:02d}-{2:02d}".format(TODAY.year, TODAY.month, TODAY.day)
     members = []
 
     for memberUrl in oparlMembers:
@@ -38,12 +41,24 @@ def getMembers(oparlMembers):
         # read membership json from oparl
         mrq = requests.get(memberUrl)
         oparlMember = mrq.json()
-        startDate = oparlMember.get('startDate')
-        endDate = oparlMember.get('endDate')
-        personUrl = oparlMember.get('person')
+        startDate = oparlMember.get("startDate")
+        endDate = oparlMember.get("endDate")
+        personUrl = oparlMember.get("person")
 
-        isCurrentMember = (startDate < currentDate) and ((not endDate) or (endDate > currentDate))
-        LOGGER.debug("%s - %s -- isCurrent: %s -- %s", startDate, endDate, isCurrentMember, personUrl)
+        if not (startDate and currentDate):
+            LOGGER.warning("missing date: %s - %s", startDate, endDate)
+            isCurrentMember = (not endDate) or (endDate > currentDate)
+        else:
+            isCurrentMember = (startDate < currentDate) and (
+                (not endDate) or (endDate > currentDate)
+            )
+        LOGGER.debug(
+            "%s - %s -- isCurrent: %s -- %s",
+            startDate,
+            endDate,
+            isCurrentMember,
+            personUrl,
+        )
 
         if isCurrentMember:
             # read person json from oparl
@@ -51,12 +66,12 @@ def getMembers(oparlMembers):
             oparlPerson = prq.json()
 
             pid = 0
-            matchObj = re.search( r'/(\d+)$', personUrl)
+            matchObj = re.search(r"/(\d+)$", personUrl)
             if matchObj:
                 pid = matchObj.group(1)
 
-            name = oparlPerson.get('name')
-            oparlEmail = oparlPerson.get('email')
+            name = oparlPerson.get("name")
+            oparlEmail = oparlPerson.get("email")
             email = ""
             party = ""
             if oparlEmail:
@@ -75,14 +90,13 @@ def getMembers(oparlMembers):
                 "pid": pid,
                 "party": party,
                 "email": email,
-                "seat": ""
+                "seat": "",
             }
-            LOGGER.debug("member %s", member)
+            LOGGER.debug("==> CURRENT MEMBER: %s", member)
 
             members.append(member)
 
-
-    with open(CONFIG_MEMBERS, 'w') as outfile:
+    with open(DEFAULT_CONFIG_MEMBERS, "w") as outfile:
         json.dump(members, outfile)
 
 
@@ -95,66 +109,95 @@ def renderJinjaTemplate(directory, template_name, **kwargs):
     return template.render(**kwargs)
 
 
-def writeSitzplanHtml():
+def getHtmlFilename(filename):
+    return (
+        "index.html"
+        if filename == DEFAULT_CONFIG_MEMBERS
+        else filename.replace(".json", ".html")
+    )
+
+
+def getNavLinks(selectedFile):
+    nav = ""
+    for seatingConfig in config.get("files"):
+        nav = nav + '<a class="nav{}" href="{}">{}</a>'.format(
+            (" active" if seatingConfig.get("file") == selectedFile else ""),
+            getHtmlFilename(seatingConfig.get("file")),
+            seatingConfig.get("date"),
+        )
+    return nav
+
+
+def writeSitzplanHtml(seatingConfig):
 
     # Read seats config
+    filename = seatingConfig.get("file")
+    subTitle = seatingConfig.get("date") + " " + seatingConfig.get("room")
     members = []
-    with open('config-members.json') as f:
+    with open(filename) as f:
         members = json.load(f)
 
     seats = {}
     for member in members:
-        seats[member.get('seat')] = member
+        seats[member.get("seat")] = member
 
-    sitzplanRows = config.get('roomLayout').get('rows')
-    sitzplanCols = config.get('roomLayout').get('columns')
+    sitzplanRows = seatingConfig.get("rows")
+    sitzplanCols = seatingConfig.get("columns")
 
     sitzplan = ""
     for row in range(sitzplanRows):
         sitzplan = sitzplan + '<div class="row">'
         for column in range(sitzplanCols):
-            seatId = '{}-{}'.format(row,column)
+            seatId = "{}-{}".format(row, column)
             personData = {}
             if seatId in seats:
                 personData = seats[seatId]
-                pName = personData.get('name')
-                sitzplan = sitzplan + '<div data-id="{}" data-party="{}" class="occ p-{}" style="{}"><span class="name">{}</span></div>'.format(
-                    personData.get('pid'),
-                    personData.get('party'),
-                    personData.get('party'),
-                    'background-image: url(https://www.stadt-muenster.de/sessionnet/sessionnetbi/im/pe{}.jpg);'.format(personData.get('pid')),
-                    pName if pName else ''
+                pName = personData.get("name")
+                sitzplan = (
+                    sitzplan
+                    + '<div data-id="{}" data-party="{}" class="occ p-{}" style="{}"><span class="name">{}</span></div>'.format(
+                        personData.get("pid"),
+                        personData.get("party"),
+                        personData.get("party"),
+                        "background-image: url(https://www.stadt-muenster.de/sessionnet/sessionnetbi/im/pe{}.jpg);".format(
+                            personData.get("pid")
+                        ),
+                        pName if pName else "",
                     )
+                )
             else:
-                sitzplan = sitzplan + '<div></div>'
-        sitzplan = sitzplan + '</div>'
+                sitzplan = sitzplan + "<div></div>"
+        sitzplan = sitzplan + "</div>"
 
     templateData = {
-        "TITLE": config.get('info').get('title'),
-        "DATE": config.get('info').get('date'),
-        "DISCLAIMER": config.get('info').get('disclaimer'),
-        "IMPRINT": config.get('info').get('imprint'),
-        "LOGO": config.get('info').get('logo'),
-        "SITZPLAN": sitzplan
+        "TITLE": config.get("info").get("title"),
+        "SUBTITLE": subTitle,
+        "NAV_LINKS": getNavLinks(filename),
+        "DISCLAIMER": config.get("info").get("disclaimer"),
+        "IMPRINT": config.get("info").get("imprint"),
+        "LOGO": config.get("info").get("logo"),
+        "SITZPLAN": sitzplan,
     }
     html = renderJinjaTemplate("template", "main.jinja2", **templateData)
 
-    with open('index.html', 'w') as outfile:
+    with open(getHtmlFilename(filename), "w") as outfile:
         outfile.write(html)
 
     print(html)
 
-if os.path.isfile(CONFIG_MEMBERS):
-    print("Members config file exists.")
+
+if os.path.isfile(DEFAULT_CONFIG_MEMBERS):
+    print("Default members config file exists.")
 else:
     # Read Gremiuem (Rat)
-    gremiumUrl = config.get('oparl').get('baseurl')
+    gremiumUrl = config.get("oparl").get("baseurl")
     LOGGER.debug("Base URL: %s", gremiumUrl)
 
     r = requests.get(gremiumUrl)
     gremium = r.json()
 
     print("Writing members file")
-    getMembers(gremium.get('membership'))
+    getMembers(gremium.get("membership"))
 
-writeSitzplanHtml()
+for seatingConfig in config.get("files"):
+    writeSitzplanHtml(seatingConfig)
